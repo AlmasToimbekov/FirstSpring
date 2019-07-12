db24ba05b8d9a4b0cd90e9096efa916d115b2239
Almas <atoimbekov@gmail.com>
2019-07-12 12:54:03 +0600

    Adding h2 database

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,6 +1,7 @@
 // tag::all[]
 // tag::allButValidation[]
 package tacos;
+import java.util.Date;
 import java.util.List;
 // end::allButValidation[]
 import javax.validation.constraints.NotNull;
@@ -12,14 +13,18 @@ import lombok.Data;
 public class Taco {
 
   // end::allButValidation[]
-//  @NotNull
-//  @Size(min=5, message="Name must be at least 5 characters long")
+  @NotNull
+  @Size(min=5, message="Name must be at least 5 characters long")
   // tag::allButValidation[]
   private String name;
   // end::allButValidation[]
-//  @Size(min=1, message="You must choose at least 1 ingredient")
+  @Size(min=1, message="You must choose at least 1 ingredient")
   // tag::allButValidation[]
   private List<String> ingredients;
+
+  private Long id;
+
+  private Date createdAt;
 
 }
 //end::allButValidation[]
